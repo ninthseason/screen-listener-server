67100415e9329bc2f4cb983db5178b0c5b765ed6
Kl1nge5 <ninthseason@qq.com>
2025-06-19 04:54:03 +0800

    v0.1.0

---
+++ main.ts
@@ -0,0 +1,57 @@
+const passwd: { [key: string]: string } = {
+  "kl1nge5": "kl1nge5",
+};
+
+Deno.serve(async (req) => {
+  if (req.method === "POST") {
+    const authHeader = req.headers.get("authorization");
+    const credentials = parseBasicAuth(authHeader);
+    if (credentials === null) {
+      return new Response(null, { status: 401 });
+    } else if (!(credentials.username in passwd)) {
+      return new Response(null, { status: 401 });
+    } else if (passwd[credentials.username] != credentials.password) {
+      return new Response(null, { status: 401 });
+    }
+    const contentType = req.headers.get("content-type") || "";
+    if (contentType.includes("application/x-www-form-urlencoded")) {
+      const formData = await req.formData();
+      const event = formData.get("event");
+      if (event === "screen-on") {
+        const kv = await Deno.openKv();
+        await kv.set(["user", "kl1nge5"], [true, Date.now()]);
+        console.log(credentials.username, "screen-on");
+      } else if (event === "screen-off") {
+        const kv = await Deno.openKv();
+        await kv.set(["user", "kl1nge5"], [false, Date.now()]);
+        console.log(credentials.username, "screen-off");
+      } else {
+        return new Response(null, { status: 415 });
+      }
+      return new Response(`{}`, {
+        headers: { "Content-Type": "application/json" },
+      });
+    } else {
+      return new Response(null, { status: 415 });
+    }
+  } else if (req.method === "GET") {
+    const kv = await Deno.openKv();
+    const rs: { [key: string]: boolean } = {};
+    for await (const entry of kv.list({ prefix: ["user"] })) {
+      rs[entry.key[1] as string] = entry.value as boolean;
+      console.log(entry.key, entry.value);
+    }
+    return new Response(JSON.stringify(rs), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+  return new Response(null, { status: 415 });
+});
+
+function parseBasicAuth(authHeader: string | null) {
+  if (!authHeader || !authHeader.startsWith("Basic ")) return null;
+  const b64 = authHeader.replace("Basic ", "");
+  const decoded = atob(b64); // Base64 解码
+  const [username, password] = decoded.split(":");
+  return { username, password };
+}
